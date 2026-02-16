116cf2de80328b8c65485b5048be2e5dd6bd3e5e
Richard <richy.prabhu7@gmail.com>
2026-02-16 12:28:07 +1100

    Initial Commit

---
+++ taskapp/taskappApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskappApp.swift
+//  taskapp
+//
+//  Created by Richard Prabhu on 2/16/26.
+//
+
+import SwiftUI
+
+@main
+struct taskappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
